299df2ee77dc7103e6c801b642b058e6155a7f2b
SolangeLL <solange.le-luel@epitech.eu>
2023-12-01 11:20:02 +0100

    :sparkles: Added home page

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -9,14 +9,38 @@ const useFetch = (endpoint) => {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'eda30c92f5mshbb5e93d0c15bd47p12086cjsne74215910f3c',
+      'X-RapidAPI-Key': 'eda30c92f5mshbb5e93d0c15bd47p12086cjsne74215910f3c' ,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     },
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    params: {
-      query: 'Python developer in Texas, USA',
-      page: '1',
-      num_pages: '1'
-    },
+    params: { ...query },
   };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options)
+      setData(response.data.data)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error)
+      alert('There is an error')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const refetch = () => {
+    setIsLoading(true)
+    fetchData()
+  }
+
+  return { data, isLoading, error, refetch }
 }
+
+export default useFetch;
